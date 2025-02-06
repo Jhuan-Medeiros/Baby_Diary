96af04e44956eb1f12a03117e0bd3d027c27d7f1
Jhuan-Medeiros <jhuan.mcordeiro@gmail.com>
2025-02-06 17:08:12 -0300

    aperfeiçoamento da tela de login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import "../Login/Login.css"
-
+import React from "react";
+import "../Login/Login.css";
 
 const Login = () => {
   return (
-    <div className='corpoLogin'>
-      <div className='tituloLogin'>
-      <img src = 'src\assets\img\logo.png' alt="logo" className='logo'/>
-        <h1>Baby Diary</h1>
+    <div className="corpoLogin">
+
+      <div className="tituloLogin">
+        <img src="src\assets\img\logo.png" alt="logo" className="logo" />
+        <h1 className="tituloSite">Baby Diary</h1>
       </div>
-      <div className='areaLogin'>
-        <div className='areasDeRegistro'>
-        <label htmlFor="rg">CPF:</label>
-        <input type="text" id='rg'/>
-        <label htmlFor="senha">Senha:</label>
-        <input type="text" id="senha" />
+      <div className="areaLogin">
+        <div className="areasDeRegistro">
+          <label htmlFor="rg">CPF:</label>
+          <input type="text" id="rg" />
+          <label htmlFor="senha">Senha:</label>
+          <input type="text" id="senha" />
+          <p>Esqueci a senha</p>
         </div>
-        <p>Esqueci a senha</p>
-        <button>Entrar</button> 
+        <button className="acessoAoSite">Entrar</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
