96c1e535f0544a8cc18d545ab0460f9b8f2e667c
Enzo151 <enzobussi3103@gmail.com>
2025-02-12 11:29:50 -0300

    começando tela Perfil

--- src/pages/Perfil/Perfil.jsx
+++ src/pages/Perfil/Perfil.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
+import "../Perfil/Perfil.css"
 
 export const Perfil = () => {
+
+
   return (
-    <div>Perfil</div>
-  )
-}
+    <>    
+    <div className="areaPerfil">
+      <h1>Perfil do Aluno</h1>
+      <div className="fotoAluno">
+        <img src="src/assets/img/zoi.jpg" alt="Foto do aluno" />
+      </div>
+    </div>
+      <div className='container'>
+      <div className="dadosAluno">
+        <h2>{aluno.nome}</h2>
+        <p><strong>Idade:</strong> {aluno.idade}</p>
+        <p><strong>Curso:</strong> {aluno.curso}</p>
+      </div>
+      </div>
+    </>
+  );
+};
+
+const aluno = {
+  nome: "Ever San Eyes",
+  idade: 6,
+  curso: "Educação Infantil",
+};
+
+export default Perfil;
+
