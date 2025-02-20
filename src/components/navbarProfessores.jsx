2737473dd7e569a7b3498b4f059ed043c70b3e27
Jhuan-Medeiros <jhuan.mcordeiro@gmail.com>
2025-02-20 17:14:05 -0300

    conserto da navbar

--- src/components/navbarProfessores.jsx
+++ src/components/navbarProfessores.jsx
@@ -1,48 +1,110 @@
-import "../pages/Home/Home.css"
-import { Link } from "react-router-dom";
-
+import "../pages/Home/Home.css";
+import { Link, useLocation } from "react-router-dom";
 
 function NavbarProfessores() {
+  const location = useLocation(); // Obtém a URL atual
+
   return (
     <div className="navbar">
       <nav>
         <strong>
           <div className="nav-item">
-            <Link to={"/home"} id="homeSelection" className="icon-link">
+            <Link
+              to="/home"
+              className={`icon-link ${
+                location.pathname === "/home" ? "active" : ""
+              }`}
+            >
               <div className="icons-abaixo">
                 <img
-                  src="src/assets/img/homeAzul.png"
-                  alt="homeAzul"
+                  src={
+                    location.pathname === "/home"
+                      ? "src/assets/img/homeAzul.png"
+                      : "src/assets/img/home.png"
+                  }
+                  alt="home"
                   id="homeAzul"
                 />
                 Home
               </div>
             </Link>
-            <Link to={"/turmas"} className="icon-link">
+
+            <Link
+              to="/turmas"
+              className={`icon-link ${
+                location.pathname === "/turmas" ? "active" : ""
+              }`}
+            >
               <div className="icons-abaixo">
-                <img src="src/assets/img/turmas.png" alt="turmas" id="turmas" />
+                <img
+                  src={
+                    location.pathname === "/turmas"
+                      ? "src/assets/img/turmasAzul.png"
+                      : "src/assets/img/turmas.png"
+                  }
+                  alt="turmas"
+                  id="turmas"
+                />
                 Turmas
               </div>
             </Link>
-            <Link to={"/perfil"} className="icon-link">
+
+            <Link
+              to="/perfil"
+              className={`icon-link ${
+                location.pathname === "/perfil" ? "active" : ""
+              }`}
+            >
               <div className="icons-abaixo">
-                <img src="src/assets/img/perfil.png" alt="perfil" id="perfil" />
+                <img
+                  src={
+                    location.pathname === "/perfil"
+                      ? "src/assets/img/perfilAzul.png"
+                      : "src/assets/img/perfil.png"
+                  }
+                  alt="perfil"
+                  id="perfil"
+                />
                 Perfil
               </div>
             </Link>
-            <Link to={"/chat"} className="icon-link">
+
+            <Link
+              to="/chat"
+              className={`icon-link ${
+                location.pathname === "/chat" ? "active" : ""
+              }`}
+            >
               <div className="icons-abaixo">
                 <img
-                  src="src/assets/img/mensagens.png"
+                  src={
+                    location.pathname === "/chat"
+                      ? "src/assets/img/mensagensAzul.png"
+                      : "src/assets/img/mensagens.png"
+                  }
                   alt="mensagens"
                   id="mensagens"
                 />
                 Chat
               </div>
             </Link>
-            <Link to={"/configuracao"} className="icon-link">
+
+            <Link
+              to="/configuracao"
+              className={`icon-link ${
+                location.pathname === "/configuracao" ? "active" : ""
+              }`}
+            >
               <div className="icons-abaixo">
-                <img src="src/assets/img/config.png" alt="config" id="config" />
+                <img
+                  src={
+                    location.pathname === "/config"
+                      ? "src/assets/img/configAzul.png"
+                      : "src/assets/img/config.png"
+                  }
+                  alt="config"
+                  id="config"
+                />
                 Configuração
               </div>
             </Link>
@@ -52,4 +114,5 @@ function NavbarProfessores() {
     </div>
   );
 }
+
 export default NavbarProfessores;
