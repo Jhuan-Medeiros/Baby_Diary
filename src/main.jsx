80889f9aac4b1b369eaca26c39650ab66381c57a
HenryBRweb <henrygabrielrb2@gmail.com>
2025-02-26 08:20:00 -0300

    .

--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,26 @@ import { createRoot } from 'react-dom/client'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 
 import Login from './pages/Login/Login'
-import { Home } from './pages/Home/Home.jsx';
+import { Home } from './pages/Home/Home';
 import { Perfil } from './pages/Perfil/Perfil';
+import Rotina from './pages/Rotina/Rotina';
 
 
 const router = createBrowserRouter([{
   path: '/home',
-  element: <Login />,
+  element: <Home />,
 },
+
 {
   path: '/',
   element: <Home />, //mudei
 },
+
+{
+  path:'/rotina',
+  element: <Rotina/>
+},
+
 {
   path: '/perfil',
   element: <Perfil/>  ,
---
+++ src/pages/Rotina/Rotina.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "../Rotina/Rotina.css";
+
+const Rotina = () => {
+    return (
+        <>
+            <div class="titulo">
+                <h1 id="cor-titsub">Rotina do aluno</h1>
+            </div>
+            <div class="subtitulo">
+                <h1 id="cor-titsub">Alimentou-se</h1>
+            </div>
+        </>
+    );
+};
+
+export default Rotina;
