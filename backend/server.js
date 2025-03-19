8f2c79f72ab643ea30457752706499a2565272b2
Jhuan-Medeiros <jhuan.mcordeiro@gmail.com>
2025-03-19 10:13:11 -0300

    inicio da criação do backend

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const usuarios = require("./models/usuarios");
+const tipos_usuarios = require("./models/tipos_usuarios");
+const routes = require("./routes/routes");
+const cookieParser = require('cookie-parser')
+
+
+tipos_usuarios.sync();
+usuarios.sync();
+
+
+const app = express();
+
+app.use(cookieParser());
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(3001, console.log("servidor rodando na porta 3001"));
